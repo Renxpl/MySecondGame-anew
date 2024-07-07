2dcfaf990ae4e11b8279e704a88689bf88e46740
Renxpl <138153283+Renxpl@users.noreply.github.com>
2024-07-07 14:14:22 +0300

    Prototyping Finisher

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    Animator enemyAnimator;
+    [SerializeField]GameObject player;
+    [SerializeField] bool finisher;
+    void Start()
+    {
+        enemyAnimator= GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = transform.position.x - player.transform.position.x;
+        if (finisher)
+        {
+            if (distance < 2)
+            {
+                player.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 0);
+                if (!enemyAnimator.GetCurrentAnimatorStateInfo(0).IsName("Finisher1"))
+                    enemyAnimator.Play("Finisher1");
+
+
+            }
+            else
+            {
+                player.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+                enemyAnimator.Play("Idle");
+            }
+        }
+    }
+}
