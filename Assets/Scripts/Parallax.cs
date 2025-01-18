9a5abab9719bcaeef571c347582998929bb24f60
Renxpl <138153283+Renxpl@users.noreply.github.com>
2025-01-18 11:06:24 +0300

    New Parallax Coding

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -17,9 +19,9 @@ public class Parallax : MonoBehaviour
     public float[] movementFactor;
     public int[] parallaxWidths;
 
-   
+    Transform cameraTransform;
 
-
+    int totalLength;
     //Parallax factor for y axis???
     void Start()
     {
@@ -30,20 +32,26 @@ public class Parallax : MonoBehaviour
 
         }
 
-        
+         cameraTransform = transform;
          parallaxMain = new GameObject[parallaxes.Length,3];
          movementInfos = new float[parallaxes.Length,2];
+         totalLength = parallaxes.Length;    
 
        
        
 
         for(int i = 0; i < parallaxes.Length; i++)
         {
+            
             //movement info[,0] is information about how many units to left or right position.
             movementInfos[i,0] = (float)parallaxWidths[i] / ((float)pixelCountToDisplayInAnUnit*2.0f);
             movementInfos[i, 1] = movementFactor[i];
 
             parallaxMain[i,1] = parallaxes[i];
+            
+            // if movement factor is 1 then do not instantiate it
+            if (movementInfos[i, 1] == 1) continue; 
+
             parallaxMain[i,0] = Instantiate(parallaxes[i]);
             parallaxMain[i, 2] = Instantiate(parallaxes[i]);
             for (int j = 0; j< 3; j += 2)
@@ -64,10 +72,58 @@ public class Parallax : MonoBehaviour
    //Update method for parallax script for making it work after camera behaviour
     void ParallaxUpdate()
     {
-
-
+        AdjustParallaxes();
 
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+    //Adjusting and Instantiating Parallaxes According to Camera Position 
+    void AdjustParallaxes()
+    {
+        for(int i = 0; i < totalLength; i++)
+        {
+            // if movement factor is 1 then do not adjust it
+            if (movementInfos[i, 1] == 1) continue;
+
+            if (cameraTransform.position.x < parallaxMain[i, 0].transform.position.x + movementInfos[i,0])
+            {
+                //Align to left
+                GameObject placeHolder = parallaxMain[i, 2];
+                parallaxMain[i, 2] = parallaxMain[i, 1];
+                parallaxMain[i, 1] = parallaxMain[i, 0];
+                parallaxMain[i, 0] = Instantiate(parallaxMain[i, 1]);
+                parallaxMain[i, 0].transform.position = new Vector2(parallaxMain[i, 1].transform.position.x - 2 * movementInfos[i, 0], parallaxMain[i, 1].transform.position.y);
+                Destroy(placeHolder);
+            }
+
+            if (cameraTransform.position.x > parallaxMain[i, 2].transform.position.x - movementInfos[i, 0])
+            {
+                //Align to right
+                GameObject placeHolder = parallaxMain[i, 0];
+                parallaxMain[i, 0] = parallaxMain[i, 1];
+                parallaxMain[i, 1] = parallaxMain[i, 2];
+                parallaxMain[i, 2] = Instantiate(parallaxMain[i, 1]);
+                parallaxMain[i, 2].transform.position = new Vector2(parallaxMain[i, 1].transform.position.x + 2 * movementInfos[i, 0], parallaxMain[i, 1].transform.position.y);
+                Destroy(placeHolder);
+
+            }
+
+        }
+       
+    }
+
+
+
 }
