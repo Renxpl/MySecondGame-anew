57624cf11961883034716bb39fc6d3599866f2f6
Renxpl <138153283+Renxpl@users.noreply.github.com>
2025-02-27 19:07:09 +0300

    Game Mechanics Optimization

--- Assets/Scripts/Player Scripts/PlayerController/PlayerRollState.cs
+++ Assets/Scripts/Player Scripts/PlayerController/PlayerRollState.cs
@@ -6,22 +6,35 @@ public class PlayerRollState : IState
 {
     Rigidbody2D playerRb;
     float rollingForceFactor = 25f;
+    float timePassed = 0;
     public void Enter()
     {
         //Debug.Log("WalkingStateStarted");
         playerRb = PlayerController.PlayerRB;
         CommandHandler.ResetNext();
+        timePassed = 0;
     }
 
 
     public void Update()
     {
+
         if (!PlayerNeededValues.IsRolling)
         {
             PlayerController.playerSM.ChangeState(PlayerNeededValues.GroundedStateForPlayer);
             return;
         }
-        if(Time.timeScale < 1)
+        timePassed += Time.deltaTime;
+        if (timePassed < 0.083f)
+        {
+            rollingForceFactor = 15f;
+        }
+        else
+        {
+            rollingForceFactor = 30f;
+        }
+        
+        if (Time.timeScale < 1)
         {
             //playerRb.velocity = new Vector2(30f * 1.5f * playerRb.gameObject.transform.localScale.x, playerRb.velocity.y);
             if(PlayerNeededValues.RollInput.x != 0)
