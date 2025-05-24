01e77a76d9a6f35b23e433dcd00b07c54293c606
Renxpl <138153283+Renxpl@users.noreply.github.com>
2025-05-24 06:22:06 +0300

    Ongoing Enemy Implementation

--- Assets/Scripts/Enemy Scripts/Renewing/Behaviours/MovementBehaviour/ChaseMovementBehaviour.cs
+++ Assets/Scripts/Enemy Scripts/Renewing/Behaviours/MovementBehaviour/ChaseMovementBehaviour.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class ChaseMovementBehaviour : IMovementBehaviour
 {
-
+    //to be deleted later
+    float range = 5f;
 
     public void Move(EnemyController self, Rigidbody2D enemyRB, Transform target)
     {
-
+        
         Vector2 dir = new Vector2(target.position.x - self.transform.position.x, 0).normalized;
-        enemyRB.velocity = self.Stats.moveSpeed * dir;
+        self.transform.localScale = new Vector2 (dir.x,1f);//Sprite Adjustment
+        //range will be next attack range which will be implemented later
+        if(Mathf.Abs(target.position.x - self.transform.position.x) > range)
+        {
+            enemyRB.velocity = self.Stats.moveSpeed * dir;
+            self.GetComponent<Animator>().Play("Walking");
+        }
+        else
+        {
+            enemyRB.velocity = 0f * dir;
+            self.GetComponent<Animator>().Play("Idle");//have to be more flexible thing
+        }
+        
 
 
 
