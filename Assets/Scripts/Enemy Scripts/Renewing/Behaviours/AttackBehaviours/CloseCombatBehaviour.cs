0dacc7d1983a0c742414c4a0a05f939c69541a23
Renxpl <138153283+Renxpl@users.noreply.github.com>
2025-06-28 22:43:08 +0300

    Ongoing Enemy Implementation

--- Assets/Scripts/Enemy Scripts/Renewing/Behaviours/AttackBehaviours/CloseCombatBehaviour.cs
+++ Assets/Scripts/Enemy Scripts/Renewing/Behaviours/AttackBehaviours/CloseCombatBehaviour.cs
@@ -41,23 +41,44 @@ public class CloseCombatBehaviour : IAttackBehaviour
             self.LockEnemySprite();
             self.SetPrePosition(self.transform.position);
             
-            yield return new WaitForSeconds(self.Combo.steps[self.AttackStep % 3].delayBeforeHit);
+            
 
             //enemyRB.WakeUp();
             //need to make first attack slower and more recognizable at the moment of attack1
             
+
             if(self.AttackStep % 3 == 0)
             {
+
+                yield return new WaitForSeconds(0.1f);
+
+                //enemyRB.velocity = new Vector2(enemyRB.transform.localScale.x * 10f, 0f);
+                enemyRB.AddForce(new Vector2(enemyRB.transform.localScale.x * 2.5f, 0f), ForceMode2D.Impulse);
+                yield return new WaitForSeconds(0.1f);
+                Vector2 currentPos = new Vector2(enemyRB.transform.position.x, enemyRB.transform.position.y);
+                float targetX = target.position.x + (Mathf.Sign(enemyRB.transform.localScale.x) * -2f);
+
+
+
+
+
+                enemyRB.MovePosition(new Vector2(targetX, currentPos.y));
+                yield return new WaitForSeconds(self.Combo.steps[self.AttackStep % 3].delayBeforeHit-0.2f);
+                enemyRB.velocity = Vector2.zero;
                 self.Combo.steps[self.AttackStep % 3].hitbox.enabled = true;
                 enemyRB.WakeUp();
-                Vector2 currentPos = new Vector2(enemyRB.transform.position.x, enemyRB.transform.position.y);
-                enemyRB.MovePosition(currentPos + (Mathf.Sign(enemyRB.transform.localScale.x)*new Vector2(2,0))); Physics2D.SyncTransforms();
+                //Vector2 currentPos = new Vector2(enemyRB.transform.position.x, enemyRB.transform.position.y);
+                //enemyRB.MovePosition(currentPos + (Mathf.Sign(enemyRB.transform.localScale.x)*new Vector2(2,0))); Physics2D.SyncTransforms();
 
 
 
             }
             else if (self.AttackStep % 3 == 1)
             {
+
+
+
+                yield return new WaitForSeconds(self.Combo.steps[self.AttackStep % 3].delayBeforeHit);
                 self.UnlockEnemySprite();
                 yield return new WaitForSeconds(0.01f);
                 self.LockEnemySprite();
@@ -80,6 +101,10 @@ public class CloseCombatBehaviour : IAttackBehaviour
             }
             else
             {
+
+
+                yield return new WaitForSeconds(0.1f);
+                enemyRB.AddForce(new Vector2(enemyRB.transform.localScale.x* 2f,0f), ForceMode2D.Impulse);
                 self.Combo.steps[self.AttackStep % 3].hitbox.enabled = true;
                 enemyRB.WakeUp();
             }
