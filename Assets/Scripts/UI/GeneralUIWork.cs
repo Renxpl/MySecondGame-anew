a899db58a6adffba32b5806b94e9747d113642e6
Renxpl <138153283+Renxpl@users.noreply.github.com>
2025-07-12 04:49:21 +0300

    UI Tests

--- Assets/Scripts/UI/GeneralUIWork.cs
+++ Assets/Scripts/UI/GeneralUIWork.cs
@@ -8,12 +8,13 @@ public class GeneralUIWork : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI comboText;
     Canvas canvas;
-    
 
+    GameObject player;
 
     void Start()
     {
         canvas = GetComponent<Canvas>();
+        player = GameObject.Find("Player");
         //canvas.pixelPerfect= true;
         
     }
@@ -21,6 +22,7 @@ public class GeneralUIWork : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        comboText.text = PlayerNeededValues.ComboCounter.ToString();        
+        comboText.text = PlayerNeededValues.ComboCounter.ToString();
+        transform.position = new Vector2( player.transform.position.x,player.transform.position.y + 3f);
     }
 }
