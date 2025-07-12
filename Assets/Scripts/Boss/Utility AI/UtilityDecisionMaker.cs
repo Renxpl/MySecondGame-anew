1f0f15652f514ef450e9955fa3a866a019ac9172
Renxpl <138153283+Renxpl@users.noreply.github.com>
2025-07-13 02:27:39 +0300

    Ongoing Boss Implementation
    Utility AI + Blackboard + State Machine Setup

--- Assets/Scripts/Boss/Utility AI/UtilityDecisionMaker.cs
+++ Assets/Scripts/Boss/Utility AI/UtilityDecisionMaker.cs
@@ -1,25 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEngine.Rendering.DebugUI;
+using System.Linq;
 
 public class UtilityDecisionMaker : MonoBehaviour
 {
     // Start is called before the first frame update
     BossTest mainScript;
     BlackboardForBoss bb;
-    
+    string[] keys = new string[Enum.GetValues(typeof(BossPurpose)).Length];
+    float[] values = new float[Enum.GetValues(typeof(BossPurpose)).Length];
+
     void Awake()
     {
         mainScript = GetComponent<BossTest>();
         bb = GetComponent<BlackboardForBoss>();
+        
     }
 
 
     void Start()
     {
+
         
     }
 
@@ -27,7 +33,11 @@ public class UtilityDecisionMaker : MonoBehaviour
     void Update()
     {
 
-       
+        Array.Sort(values, keys);
+        Array.Reverse(values);
+        Array.Reverse(keys);
+
+
 
 
 
@@ -48,7 +58,7 @@ public class UtilityDecisionMaker : MonoBehaviour
         return Mathf.Clamp01(1f - (dist / maxRange));
     }
 
-    float ScoreFromHp(Vector2 selfPos, Vector2 playerPos, float hp)
+    float ScoreFromHp(Vector2 selfPos, Vector2 playerPos, float hp, float maxHp)
     {
         float dist = Vector2.Distance(selfPos, playerPos);
         // 1 – (dist / maxRange) ile 0–1 aralýðýna indirgeriz, sonra clamp
@@ -56,24 +66,24 @@ public class UtilityDecisionMaker : MonoBehaviour
         return Mathf.Clamp01(value);
     }
 
-    float ScoreForAttack(Vector2 selfPos, Vector2 playerPos, float maxRange, float hp)
+    float ScoreForAttack(Vector2 selfPos, Vector2 playerPos, float maxRange, float hp, float maxHp)
     {
 
         float dist = Vector2.Distance(selfPos, playerPos);
       
 
-        float value = 1f - (dist / maxRange) ;
+        float value = (maxRange / dist)* 0.8f + hp * 0.2f / maxHp;
         return Mathf.Clamp01(value);
 
     }
 
-    float ScoreForSpecialAttack(Vector2 selfPos, Vector2 playerPos, float hp)
+    float ScoreForSpecialAttack(Vector2 selfPos, Vector2 playerPos, float hp, float maxHp)
     {
 
         float dist = Vector2.Distance(selfPos, playerPos);
 
 
-        float value = 0f;
+        float value = 1f - hp/maxHp;
         return Mathf.Clamp01(value);
 
 
